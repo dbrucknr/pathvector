75fa7d108a8adccf37a1afd7a8d0c417cea0a553
Derek Bruckner <dbrucknr@umich.edu>
2026-06-24 15:26:50 -0400

    Prepare to publish pathvector-client

--- pathvector-client/build.rs
+++ pathvector-client/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let manifest = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let proto_root = manifest.parent().unwrap().join("proto");
+    let proto_root = manifest.join("proto");
 
     tonic_prost_build::configure()
         .build_client(true)
